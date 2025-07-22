5853e41b8834d9734e08fb8e02efddd0629b78c4
Gazerss <87362827+Gazerss@users.noreply.github.com>
2025-07-23 01:15:52 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace AddressParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ButtonLoadFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // ButtonLoadFile
+            // 
+            this.ButtonLoadFile.Location = new System.Drawing.Point(146, 51);
+            this.ButtonLoadFile.Name = "ButtonLoadFile";
+            this.ButtonLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLoadFile.TabIndex = 0;
+            this.ButtonLoadFile.Text = "讀取檔案";
+            this.ButtonLoadFile.UseVisualStyleBackColor = true;
+            this.ButtonLoadFile.Click += new System.EventHandler(this.ButtonLoadFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 450);
+            this.Controls.Add(this.ButtonLoadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button ButtonLoadFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using AddressParser.Helpers;
+using System;
+using System.Windows.Forms;
+
+namespace AddressParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLoadFile_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel 檔案 (*.xlsx)|*.xlsx",
+                Title = "選擇 Excel 檔案"
+            };
+
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                ExcelAddressTranslator.TranslateColumnAtoB(path);
+
+                MessageBox.Show("地址翻譯完成！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            return;
+        }
+    }
+}
